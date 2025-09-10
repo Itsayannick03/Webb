ff59b430b5b30ae539d515e14e5fc2ea034f0139
Itsyannick03 <yannickwinkler3@gmail.com>
2025-09-10 19:45:39 +0200

    add backend project structure and db connectionn

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,17 @@
+require("dotenv").config();
 const connectDB =require("./db");
+const express = require("express");
 
+const app = express();
+app.use(express.json());
+
+//Connecting to database
 connectDB();
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
