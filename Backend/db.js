2cd3216b42547b833271fa7283ac80d918ae82a6
ItsaYannick <jaxterlp@gmail.com>
2025-09-10 19:45:39 +0200

    add backend project structure and db connectionn

--- Backend/db.js
+++ Backend/db.js
@@ -2,15 +2,16 @@ const mongoose = require("mongoose");
 
 async function connectDB()
 {
+
     try
     {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("%cMongoDB connected", "color: green");
+        console.log("MongoDB connected");
 
     }
     catch(err)
     {
-        console.error("%cDatabase connection failed:", "color: red", err.messege);
+        console.error("Database connection failed:",err.messege);
         process.exit(1);
     }
 }
