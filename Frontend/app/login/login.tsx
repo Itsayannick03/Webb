544849e9c8fad30c5b2ad1f96bdd522bb7c3b708
Fadde1 <jafa22my@student.ju.se>
2025-09-14 00:27:05 +0200

    feat(login): add basic React login page

--- Frontend/app/login/login.tsx
+++ Frontend/app/login/login.tsx
@@ -1,6 +1,23 @@
+import "./Login.css";
+import { FaUser, FaLock } from "react-icons/fa";
 export function Login()
 {
     return(
-        <p>Login page</p>
+        //<p>Login page</p>
+        <div className="wrapper">
+            <form action="" >
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="email" placeholder="Email" required/>
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required/>
+                    <FaLock className="icon"/>
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    
     )
 }
