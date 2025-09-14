1a28e77be9074b07357a0658cab065e0cf5568b4
Itsayannick03 <yannickwinkler3@gmail.com>
2025-09-14 21:53:50 +0200

    Centered logos besides text

--- Frontend/app/footer.tsx
+++ Frontend/app/footer.tsx
@@ -3,19 +3,23 @@ import '/home/yyannick/Webb/Frontend/app/styles/footer.css'
 export function Footer()
 {
     return(
-        <div className='footer'>
-            <img className='footerLogo'  src="logo.png" alt="Mane Attraction text" width={160} />
-            <div className='footerText'>
-                <p>Exempelgatan 12</p>
-                <p>123 45 Jönköping</p>
-                <p>010-123 45 67</p>
-                <p>kontakt@maneattraction.se</p>
-                <p>Mån-Fre 09:00-18:00</p>
-                
-  
+        <footer className="footer">
+        <img id="img1" src="logo.png" alt="Mane Attraction" className="footer-logo" />
+
+        <div className="footer-text">
+            <div className="footer-row">
+            <p>Exempelgatan 12</p>
+            <p>123 45 Jönköping</p>
+            <p>010-123 45 67</p>
             </div>
-            <img className='footerLogo' src="logo.png" alt="Mane Attraction text" width={160} />
+            <div className="footer-row">
+            <p>kontakt@maneattraction.se</p>
+            <p>Mån–Fre 09:00–18:00</p>
+            </div>
         </div>
+
+        <img id="img1" src="logo.png" alt="Mane Attraction" className="footer-logo" />
+        </footer>
 
     )
 }
