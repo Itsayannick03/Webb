7f2271a059f99e6966d6d430bbb0464750a60fde
Itsayannick03 <yannickwinkler3@gmail.com>
2025-09-15 21:24:29 +0200

    Fixed spacing

--- Frontend/app/registration/registration.tsx
+++ Frontend/app/registration/registration.tsx
@@ -12,29 +12,41 @@ export function Registration()
   <form action="#">
     <div className="registration">
       <div className = "h1">
-      <h1>Registration</h1>
+      
       </div>
-      <hr />
       <div className = "border">
-          <label htmlFor="firstname"><b>First Name <div className = "first"><FaIdCard /></div></b></label>
-          <input type="text" placeholder="Enter First Name" name="firstname" id="firstname" required/>
+          <div>
+            <label htmlFor="firstname"><b>First Name </b></label>
+            <input type="text" placeholder="Enter First Name" name="firstname" id="firstname" required/>
+          </div>
+          
+          <div>
+            <label htmlFor="lastname"><b>Last Name </b></label>
+            <input type="text" placeholder="Enter Last Name" name="lastname" id="lastname" required/>
+          </div>
+          
+          <div>
+            <label htmlFor="email"><b>Email </b></label>
+            <input type="text" placeholder="Enter email" name="email" id="email" required/>
+          </div>
+          
+          <div>
+            <label htmlFor="phonenumber"><b>Phone Number </b></label>
+            <input type="text" placeholder="Enter Phone Number" name="phonenumber" id="phonenumber" required/>
+          </div>
+          
+          <div>
+            <label htmlFor="psw"><b>Password </b></label> 
+            <input type="password" placeholder="Enter Password" name="psw" id="psw" required /> 
+          </div>
+          
+          <div>
+            <label htmlFor="psw-repeat"><b>Repeat Password </b></label>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required/>
+          </div>
+          <button type="submit" className="registerbtn">Register</button>
 
-          <label htmlFor="lastname"><b>Last Name <div className = "user"><FaUser /></div></b></label>
-          <input type="text" placeholder="Enter Last Name" name="lastname" id="lastname" required/>
-
-          <label htmlFor="email"><b>Email <div className="mail"><IoMdMail /></div></b></label>
-          <input type="text" placeholder="Enter email" name="email" id="email" required/>
-
-          <label htmlFor="phonenumber"><b>Phone Number <div className="phone"><FaPhone /></div></b></label>
-          <input type="text" placeholder="Enter Phone Number" name="phonenumber" id="phonenumber" required/>
-
-          <label htmlFor="psw"><b>Password <div className="icon"><FaKey /></div></b></label> 
-          <input type="password" placeholder="Enter Password" name="psw" id="psw" required /> 
-
-          <label htmlFor="psw-repeat"><b>Repeat Password <div className="icon"><FaKey /></div></b></label>
-          <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required/>
         </div>
-      <button type="submit" className="registerbtn">Register</button>
 
       <div className="signin">
         <p>Already have an account? <a href="/login">Sign in</a>.</p>
