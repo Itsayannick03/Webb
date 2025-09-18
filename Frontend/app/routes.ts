b188c461997eb77405800e6d2660ad9c96316cbf
waal22el <158295274+waal22el@users.noreply.github.com>
2025-09-18 21:57:56 +0200

    booking site

--- Frontend/app/routes.ts
+++ Frontend/app/routes.ts
@@ -1,4 +1,4 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 
-export default [index("routes/home.tsx"), route("login", "routes/login.tsx"), route("registration", "routes/registration.tsx")] satisfies RouteConfig;
+export default [index("routes/home.tsx"), route("login", "routes/login.tsx"), route("registration", "routes/registration.tsx"), route("booking", "routes/booking.tsx")] satisfies RouteConfig;
