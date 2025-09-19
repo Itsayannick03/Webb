364f76b44f5eee6e2c57a7320bd75929eeb7be8c
waal22el <158295274+waal22el@users.noreply.github.com>
2025-09-19 15:02:48 +0200

    booking site fixed

--- Frontend/app/booking/booking.tsx
+++ Frontend/app/booking/booking.tsx
@@ -1,9 +1,9 @@
 import '../booking/booking.css';
 
-import { BsScissors } from "react-icons/bs";
-import { LiaBrushSolid } from "react-icons/lia";
-import { PiHairDryerBold } from "react-icons/pi";
-import { GiComb } from "react-icons/gi";
+import { PiScissorsThin } from "react-icons/pi";
+import { PiPaintBrushHouseholdThin } from "react-icons/pi";
+import { PiHairDryerThin } from "react-icons/pi";
+import { PiOfficeChairThin } from "react-icons/pi";
 export function Booking() 
 {
     return(
@@ -14,25 +14,33 @@ export function Booking()
             <div className="border">
             <div className="button">
 
+            <a href="#">
             <button type="button" className="haircut">
-            <div className="icon"><BsScissors /></div>
+            <div className="icon"><PiScissorsThin /></div>
             <div className="text">Haircut </div>
             </button>
+            </a>
             
+            <a href="#">
             <button type="button" className="color">
-            <div className="icon"><LiaBrushSolid /></div>
+            <div className="icon"><PiPaintBrushHouseholdThin /></div>
             <div className="text">Color </div>
             </button>
+            </a>
 
+            <a href="#">
             <button type="button" className="hairandcolor">
-            <div className="icon1"><GiComb /></div>
+            <div className="icon1"><PiOfficeChairThin /></div>
             <div className="text1">Haircut & Color </div>
             </button>
+            </a>
 
+            <a href="#">
             <button type="button" className="styling">
-            <div className="icon"><PiHairDryerBold /></div>
+            <div className="icon"><PiHairDryerThin /></div>
             <div className="text">Styling </div>
             </button>
+            </a>
             </div>
             </div>
         </div>
