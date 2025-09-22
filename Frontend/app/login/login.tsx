8aa2275bb59da7a5e3c6d01995a4dd892b94e002
Fadde1 <jafa22my@student.ju.se>
2025-09-22 22:30:08 +0200

    feat(login) started with inlog function

--- Frontend/app/login/login.tsx
+++ Frontend/app/login/login.tsx
@@ -1,7 +1,26 @@
 import "./Login.css";
 import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
 export function Login()
+
 {
+  const name = (document.getElementById("email") as HTMLInputElement).value; // variable som är det usern skriver email
+  const password = (document.getElementById("password") as HTMLInputElement).value; // samma men för password 
+
+  async function checkPassword(email: string, password: string) {
+    try {
+      const response = await fetch(
+        `/passwordcheck?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`,
+        {
+          method: "GET",
+        }
+      );
+    } catch (err) {
+      // optional: hantera fel här
+    }
+  }
+
+
     return(
         
         <div className="wrapper">
@@ -9,11 +28,11 @@ export function Login()
             <form action="" >
                 <h1 className="Login-title">Login</h1>
                 <div className="input-box">
-                    <input type="email" placeholder="Email" required/>
+                    <input id="email" type="email" placeholder="Email" required/>
                     <FaUser className="icon"/>
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
+                    <input id="password" type="password" placeholder="Password" required/>
                     <FaLock className="icon"/>
                 </div>
                 <div className="register-link">
