055851594696f6c056df4ffec3a2e65e5eb9d26c
Fadde1 <jafa22my@student.ju.se>
2025-10-02 16:29:52 +0200

    added calender folder and files

---
+++ Frontend/app/calender/calender.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./calender.css";
+
+
+return function calender ()
+
+
+{
+    return (
+
+        <div className='calender'>
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+} 
+
+
+
