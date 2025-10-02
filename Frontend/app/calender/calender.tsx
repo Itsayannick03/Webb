61c8a419ce68416596b69b2c7daa64d9a667739b
Fadde1 <jafa22my@student.ju.se>
2025-10-02 23:43:32 +0200

    feat(calendar): initial TSX structure + navigation with react - icons – CSS to follow

--- Frontend/app/calender/calender.tsx
+++ Frontend/app/calender/calender.tsx
@@ -1,17 +1,50 @@
 import React from "react";
 import "./calender.css";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import { useState } from "react";
+
+export function Calender () {
+
+const [cursor, setCursor] = useState(new Date());
+
+const prev = () =>
+    setCursor(d => new Date(d.getFullYear(), d.getMonth() - 1, 1));
+  const next = () =>
+    setCursor(d => new Date(d.getFullYear(), d.getMonth() + 1, 1));
 
 
-return function calender ()
 
-
-{
     return (
 
-        <div className='calender'>
-            
+            <div className="calender-header">
+            <header>
+
+                <button type = "button" className="nav-btn" onClick= {prev} aria-label="Previous month">
+            <GrPrevious />
+        </button>
+
+         <h2 className="month-label">{}</h2>
+
+            <button type = "button" className="nav-btn" onClick={next}  aria-label="Next month">
+          <GrNext />
+        </button>
+            </header>
+
+        <div className="days">
+        <div>Mon</div>
+        <div>Tue</div>
+        <div>Wen</div>
+        <div>Thu</div>
+        <div>Fri</div>
+        <div>Sat</div>
+        <div>Sun</div>
 
 
+
+
+
+
+        </div>
 
 
 
@@ -37,14 +70,7 @@ return function calender ()
 
 
 
-    )
-
-
-
-
-
-
-
+    ); 
 
 
 
