2814496917d52774b67c774842afa787f004c41e
waal22el <158295274+waal22el@users.noreply.github.com>
2025-10-03 15:13:35 +0200

    service site fixed

--- Frontend/app/booking/booking.tsx
+++ Frontend/app/booking/booking.tsx
@@ -3,45 +3,31 @@ import '../booking/booking.css';
 import { PiScissorsThin } from "react-icons/pi";
 import { PiPaintBrushHouseholdThin } from "react-icons/pi";
 import { PiHairDryerThin } from "react-icons/pi";
-import { PiOfficeChairThin } from "react-icons/pi";
-export function Booking() 
-{
-    return(
-        <div className="bookingsite">
-            <div className="head">
-            <h1><b>What do you want to do?</b></h1>
+export function Booking() {
+    return (
+        <div>
+            <div className="header">
+                <h1>What do you want to do?</h1>
             </div>
-            <div className="border">
-            <div className="button">
 
-            <a href="#">
-            <button type="button" className="haircut">
-            <div className="icon"><PiScissorsThin /></div>
-            <div className="text">Haircut </div>
-            </button>
-            </a>
-            
-            <a href="#">
-            <button type="button" className="color">
-            <div className="icon"><PiPaintBrushHouseholdThin /></div>
-            <div className="text">Color </div>
-            </button>
-            </a>
-
-            <a href="#">
-            <button type="button" className="hairandcolor">
-            <div className="icon1"><PiOfficeChairThin /></div>
-            <div className="text1">Haircut & Color </div>
-            </button>
-            </a>
-
-            <a href="#">
-            <button type="button" className="styling">
-            <div className="icon"><PiHairDryerThin /></div>
-            <div className="text">Styling </div>
-            </button>
-            </a>
+            <div className="bookingsite">
+                <div className='button-wrapper'>
+                    <div className='button-container'>
+                        <PiScissorsThin size={50} />
+                        <button>Haircut</button>
+                    </div>
+                    <div className='button-container'>
+                        <PiPaintBrushHouseholdThin size={50}/>
+                        <button>Color</button>
+                    </div>
+                    <div className='button-container'>
+                        <PiHairDryerThin size={50} />
+                       <button>Styling</button>
+                    </div>
+                </div>
             </div>
+            <div className="confirm-button">
+                <button id="confirm" disabled>Confirm</button>
             </div>
         </div>
     )
