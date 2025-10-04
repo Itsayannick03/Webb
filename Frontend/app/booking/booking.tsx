a4714b2ac8685a68bd5871b2d3ee19c46bf716fc
waal22el <158295274+waal22el@users.noreply.github.com>
2025-10-04 21:00:06 +0200

    service site utdated

--- Frontend/app/booking/booking.tsx
+++ Frontend/app/booking/booking.tsx
@@ -1,9 +1,16 @@
 import '../booking/booking.css';
-
+import { useState } from "react";
 import { PiScissorsThin } from "react-icons/pi";
 import { PiPaintBrushHouseholdThin } from "react-icons/pi";
 import { PiHairDryerThin } from "react-icons/pi";
+
 export function Booking() {
+    const [haircut, setHaircut] = useState(false);
+    const [color, setColor] = useState(false);          //useState tracks if the services are selected
+    const [styling, setStyling] = useState(false);
+
+    const Confirm = haircut || color || styling;        // confirm is true if any of the services are selected
+
     return (
         <div>
             <div className="header">
@@ -12,23 +19,30 @@ export function Booking() {
 
             <div className="bookingsite">
                 <div className='button-wrapper'>
-                    <div className='button-container'>
+                    <div                                                            // ? equivalent to if..else, if haircut true selected
+                        className={`button-container ${haircut ? 'selected' : ''}`} // $ equivalent to + in python 
+                        //() => equivalent to function(), when clicked setHaircut to not styling
+                        onClick={() => setHaircut(!haircut)}>
                         <PiScissorsThin size={50} />
                         <button>Haircut</button>
                     </div>
-                    <div className='button-container'>
-                        <PiPaintBrushHouseholdThin size={50}/>
+                    <div
+                        className={`button-container ${color ? 'selected' : ''}`}
+                        onClick={() => setColor(!color)}>
+                        <PiPaintBrushHouseholdThin size={50} />
                         <button>Color</button>
                     </div>
-                    <div className='button-container'>
+                    <div                                                             
+                        className={`button-container ${styling ? 'selected' : ''}`} 
+                        onClick={() => setStyling(!styling)}>
                         <PiHairDryerThin size={50} />
-                       <button>Styling</button>
+                        <button>Styling</button>
                     </div>
                 </div>
             </div>
-            <div className="confirm-button">
-                <button id="confirm" disabled>Confirm</button>
-            </div>
-        </div>
+            <div className="confirm-button"> 
+                <button disabled={!Confirm}>Confirm</button>  
+            </div> 
+        </div>   //button is disabled when confirm is false
     )
 }
