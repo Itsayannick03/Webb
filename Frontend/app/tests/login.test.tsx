02515a34aeba7b1c05a0e6cb52ceadb78f1effcb
Fadde1 <jafa22my@student.ju.se>
2025-10-11 16:46:12 +0200

    added render test for login component

---
+++ Frontend/app/tests/login.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Login } from "../login/login";
+
+test("renders Login title, inputs, and button", () => {
+  // RENDER the component
+  render(<Login />);
+
+  //  elements expected to see
+  expect(screen.getByText("Login")).toBeInTheDocument();                 // title
+  expect(screen.getByPlaceholderText("Email")).toBeInTheDocument();      // email input
+  expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();   // password input
+  expect(screen.getByRole("button", { name: /login/i })).toBeInTheDocument(); // button
+});
