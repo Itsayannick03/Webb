5a68b84bac420598799286e53bd0ef6eeb5b1f26
Fadde1 <jafa22my@student.ju.se>
2025-10-11 14:31:14 +0200

    added the layout of the homepage (jsx)

--- Frontend/app/Homepage/homepage.tsx
+++ Frontend/app/Homepage/homepage.tsx
@@ -9,38 +9,16 @@ export function Homepage() {
 
 
 
-
-
-
-
-
-
     return (
 
         <div className="homepage">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </div>
-
-
-
-
-
-
-
+            <div className="home-container">
+                <div className="home-content">
+                    <h1 className="home-title">We are the Mane Attraction</h1>
+                    <a className="home-button" href="/login">Book now</a>
+                    </div>
+                 </div>
+            </div>
 
     )
 
