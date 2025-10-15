43b491901cb81f0184f20b0211d7bb9a989a1a55
Yannick Winkler <yannickwinkler3@gmail.com>
2025-10-15 11:45:17 +0200

    Changed book now path

--- Frontend/app/welcome/welcome.tsx
+++ Frontend/app/welcome/welcome.tsx
@@ -1,15 +1,35 @@
 import React from "react";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+
 
 import '../styles/homepage.css'
 
 export function Welcome() {
+  const [isLoggedIn, setLogin] = useState(false);
+  
+  function getUser()
+  {
+    const user = Cookies.get("user");
+
+    if(user)
+      setLogin(true);
+    else
+      setLogin(false);
+  }
+
+   useEffect(() => {
+    getUser();
+      }, []);
+
   return (
 
         <div className="homepage">
             <div className="home-container">
                 <div className="home-content">
                     <h1 className="home-title">We are <br/>The Mane Attraction</h1>
-                    <a className="home-button" href="/booking">Book now</a>
+                    {isLoggedIn ?  <a className="home-button" href="/booking">Book now</a> :  <a className="home-button" href="/login">Book Now</a>}
+                   
                     </div>
                  </div>
             </div>
